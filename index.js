5c41ae9c6c85d920599845ada9792b98d54466de
marcelhannouti <mel>
2022-05-04 10:23:41 +0200

    primer commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+// undefined
+let nombre = `marc`
+nombre = "luis"
+//string 
+let string = "hola"
+
+//number
+
+let variablenumber =123
+//null
+ let variable = null 
+
+ //booleano
+ let  usuario  =true
+
+ //object
+let userobject = {
+    // un objecto tiene propiedades
+nombre: `marc`,
+edad: 38,
+validado: true,
+soy: `hombre` ,
+direccion:{
+    calle:`beethoven`
+}
+
+}
+//function
+
+let devolver = ( edad ,userobject) =>{
+if (edad> userobject.edad){
+
+}else{
+
+}
+}
+
+let segunadaduncion = ()=>{
+    console.log(`segunda funciona`)
+}
+
+segunadaduncion()
+
+userobject.validado(18,userobject){
+
+}
+let number1 =3
+let number2 =5
+
+
+let suma = (number1,number2) =>{
+    if (number1 != null && number2 !=null) {
+        let sumatotal =  number1 + number2
+        return sumatotal
+    } else {
+        return false
+    }
+}
+
+
